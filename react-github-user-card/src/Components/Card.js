25e0a7e0adbe5ae39f908850401aa210441a9ee7
Victoria Blancas <vikki6688@gmail.com>
2023-01-03 10:24:48 -0500

    added some styles

--- react-github-user-card/src/Components/Card.js
+++ react-github-user-card/src/Components/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Card(props) {
     //step 5 - see when it's rendering and check if props was passed down ... still empty but component works
@@ -6,7 +7,7 @@ function Card(props) {
     console.log("props", props)
 
     return (
-        <div>
+        <div className="card-wrapper">
             {/* stretch 1h.a use search prop (searchUser state) to show name of who you search -- moved it to App so it's above the search bar */}
             {/* <h4>{props.search}'s Github Follow List</h4> */}
 
@@ -14,16 +15,22 @@ function Card(props) {
             {/* for each card, I want to get and display its id, avatar, and login info */}
 
             {props.card.map((eachCard) => (
-                <div key={eachCard.id} className="follower">
-                    <img 
-                        src={eachCard.avatar_url} 
-                        alt={eachCard.login} 
-                        width="200"
-                    />
-                    
+                <div key={eachCard.id} className="card-follower">
+                    {/* <Link to={eachCard.html_url}> */}
+                    <a href={eachCard.html_url}>
+                        <img className="card-image"
+                            src={eachCard.avatar_url} 
+                            alt={eachCard.login} 
+                            // width="200"
+                        />
+                    </a>
+                    {/* </Link> */}
                     <div>
                         {/* step 6a: username */}
-                        <h4>{eachCard.login}</h4>
+                        
+                        {/* <a href={eachCard.html_url}> */}
+                         <h4>{eachCard.login}</h4>
+                        {/* </a> */}
                     </div>
                 </div>
             ))}
